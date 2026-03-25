4e2e782444c6d4b21f9d5f0727b02776d044306a
11Kolko11 <kn24-i.trush@nubip.edu.ua>
2026-03-25 10:46:50 +0200

    Create first React lab

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Tyt napusano</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import './Main.css';
+
+function Main() {
+    return (
+        <main className="main-content">
+            <h1>Knopka</h1>
+            <button onClick={() => alert(":0")}>Click</button>
+        </main>
+    );
+}
+
+export default Main;
